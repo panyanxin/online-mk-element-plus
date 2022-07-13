d62fb5222f8eefe31abb15bbede014081aaf9935
panyanxin <1205307815@qq.com>
2022-07-13 16:25:36 +0800

    二次封装element-puls静态文件

---
+++ assets/index.36b1237b.js
@@ -0,0 +1,1 @@
+import{d as a,a as s,o as c,f as r,u as t}from"./index.fe4b2a60.js";const p=a({__name:"index",setup(_){let o=e=>{console.log(e)};return(e,l)=>{const n=s("m-choose-area");return c(),r(n,{onChange:t(o)},null,8,["onChange"])}}});export{p as default};
